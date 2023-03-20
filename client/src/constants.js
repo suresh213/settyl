bcc1b6bed9c5897c625f0d564c358f98a180ca2c
suresh213 <asuresh52012@gmail.com>
2023-03-21 03:26:26 +0530

    Add. Constants for employee

---
+++ client/src/constants.js
@@ -0,0 +1,13 @@
+export const initialEmployeeDetails = {
+  name: null,
+  age: null,
+  department: null,
+  address: null,
+  status: null,
+};
+
+export const employeeStatus = [
+  "Remote Location",
+  "Contract Employee",
+  "Full-Time",
+];
