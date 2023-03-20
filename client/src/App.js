4469e92bf23cf2b3bec0b7e174c18f6b406b7786
suresh213 <asuresh52012@gmail.com>
2023-03-21 03:26:52 +0530

    Add. Dashboard in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React, { Fragment, useEffect } from "react";
 import "./App.css";
-import {
-  BrowserRouter as Router,
-  Route,
-  Switch,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 import store from "./store";
+import Dashboard from "./components/Dashboard/Dashboard";
 
 require("dotenv").config();
 
@@ -15,7 +12,9 @@ const App = () => {
     <Provider store={store}>
       <Router>
         <Fragment>
-          <Switch></Switch>
+          <Switch>
+            <Route path="/" component={Dashboard} />
+          </Switch>
         </Fragment>
       </Router>
     </Provider>
