ce0afa9b1a2b3fb16e271174fa4b2987930ea8ca
suresh213 <asuresh52012@gmail.com>
2023-03-23 02:23:46 +0530

    Change path

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { Provider } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./components/Dashboard/Dashboard";
-import Employees from "./components/Employees/Employees";
 import store from "./store";
 
 require("dotenv").config();
@@ -14,9 +13,8 @@ const App = () => {
       <Router>
         <Fragment>
           <Switch>
+            <Route path="/" component={Dashboard} />
             <Route path="/dashboard" component={Dashboard} />
-            <Route path="/" component={Employees} />
-            <Route path="/employees" component={Employees} />
           </Switch>
         </Fragment>
       </Router>
